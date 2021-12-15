71befe980805ba265cd2ae2d1ef5c4885c5e28cf
aritznieto <aritz.nieto@opendeusto.es>
2021-12-15 19:58:59 +0100

    commit

--- src/ventanas/AgregarSaldo.java
+++ src/ventanas/AgregarSaldo.java
@@ -14,6 +14,9 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class AgregarSaldo extends JFrame {
 
@@ -30,22 +33,28 @@ public class AgregarSaldo extends JFrame {
 	 */
 	public AgregarSaldo(Usuario u) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 531, 281);
+		setBounds(100, 100, 447, 419);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblCantidadDeSaldo = new JLabel("Cantidad de saldo para agregar (double) :");
-		lblCantidadDeSaldo.setBounds(15, 39, 372, 20);
+		lblCantidadDeSaldo.setForeground(new Color(204, 204, 51));
+		lblCantidadDeSaldo.setFont(new Font("Mongolian Baiti", Font.PLAIN, 17));
+		lblCantidadDeSaldo.setBounds(45, 102, 372, 20);
 		contentPane.add(lblCantidadDeSaldo);
 		
 		textField = new JTextField();
-		textField.setBounds(15, 103, 238, 26);
+		textField.setBounds(45, 138, 265, 26);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.setBackground(new Color(0, 0, 0));
+		btnAceptar.setFont(new Font("Mongolian Baiti", Font.PLAIN, 17));
+		btnAceptar.setForeground(new Color(204, 204, 51));
 		btnAceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -61,10 +70,13 @@ public class AgregarSaldo extends JFrame {
 				dispose();
 			}
 		});
-		btnAceptar.setBounds(15, 162, 115, 29);
+		btnAceptar.setBounds(55, 180, 115, 29);
 		contentPane.add(btnAceptar);
 		
 		JButton btnVolver = new JButton("Volver");
+		btnVolver.setBackground(new Color(0, 0, 0));
+		btnVolver.setFont(new Font("Mongolian Baiti", Font.PLAIN, 17));
+		btnVolver.setForeground(new Color(204, 204, 51));
 		btnVolver.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
@@ -72,16 +84,50 @@ public class AgregarSaldo extends JFrame {
 				
 			}
 		});
-		btnVolver.setBounds(191, 162, 115, 29);
+		btnVolver.setBounds(195, 180, 115, 29);
 		contentPane.add(btnVolver);
 		
 		JLabel lblSaldo = new JLabel("Saldo:");
-		lblSaldo.setBounds(349, 16, 69, 20);
+		lblSaldo.setFont(new Font("Mongolian Baiti", Font.PLAIN, 17));
+		lblSaldo.setForeground(new Color(204, 204, 51));
+		lblSaldo.setBounds(270, 45, 69, 20);
 		contentPane.add(lblSaldo);
 		
 		JLabel label = new JLabel(u.getSaldo()+"");
-		label.setBounds(409, 16, 69, 20);
+		label.setForeground(new Color(204, 204, 51));
+		label.setBounds(329, 45, 38, 20);
 		contentPane.add(label);
+		
+		JLabel lblNewLabel = new JLabel("@KapitalFilms_");
+		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Logo Insta.jpg"));
+		lblNewLabel.setForeground(new Color(204, 204, 51));
+		lblNewLabel.setFont(new Font("Mongolian Baiti", Font.PLAIN, 16));
+		lblNewLabel.setBounds(210, 277, 147, 20);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("@KapitalFilms_");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Logo twitter.png"));
+		lblNewLabel_1.setForeground(new Color(204, 204, 51));
+		lblNewLabel_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 16));
+		lblNewLabel_1.setBounds(210, 317, 147, 20);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel(u.getNombreUsuario()+"");
+		lblNewLabel_2.setFont(new Font("Mongolian Baiti", Font.PLAIN, 30));
+		lblNewLabel_2.setForeground(new Color(204, 204, 51));
+		lblNewLabel_2.setBounds(45, 29, 253, 44);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("@");
+		lblNewLabel_2_1.setForeground(new Color(204, 204, 51));
+		lblNewLabel_2_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 30));
+		lblNewLabel_2_1.setBounds(15, 29, 38, 44);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\Alumno\\Downloads\\logo.PNG"));
+		lblNewLabel_3.setBounds(0, 245, 195, 114);
+		contentPane.add(lblNewLabel_3);
 		
 		
 	}
--- src/ventanas/Principal.java
+++ src/ventanas/Principal.java
@@ -79,7 +79,7 @@ public class Principal extends JFrame {
 		setTitle("Principal");
 		ArrayList<Articulo> articulos = bd.SelectData.selectArticulosDeArticulos();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1052, 1020);
+		setBounds(100, 100, 1052, 1029);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -105,33 +105,33 @@ public class Principal extends JFrame {
 		});
 		btnNewButton.setForeground(new Color(204, 204, 51));
 		btnNewButton.setBackground(Color.BLACK);
-		btnNewButton.setBounds(72, 130, 139, 46);
+		btnNewButton.setBounds(69, 112, 139, 46);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_3 = new JButton("ABOUT US");
 		btnNewButton_3.setFont(new Font("Mongolian Baiti", Font.PLAIN, 20));
 		btnNewButton_3.setForeground(new Color(204, 204, 51));
 		btnNewButton_3.setBackground(new Color(0, 0, 0));
-		btnNewButton_3.setBounds(841, 130, 174, 46);
+		btnNewButton_3.setBounds(846, 112, 174, 46);
 		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_1 = new JButton("SERIES");
 		btnNewButton_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 20));
 		btnNewButton_1.setForeground(new Color(204, 204, 51));
 		btnNewButton_1.setBackground(new Color(0, 0, 0));
-		btnNewButton_1.setBounds(216, 130, 147, 46);
+		btnNewButton_1.setBounds(216, 112, 147, 46);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("DOCUMENTALES");
 		btnNewButton_2.setFont(new Font("Mongolian Baiti", Font.PLAIN, 20));
 		btnNewButton_2.setForeground(new Color(204, 204, 51));
 		btnNewButton_2.setBackground(new Color(0, 0, 0));
-		btnNewButton_2.setBounds(378, 130, 193, 46);
+		btnNewButton_2.setBounds(378, 112, 193, 46);
 		contentPane.add(btnNewButton_2);
 		
 		JDesktopPane desktopPane = new JDesktopPane();
 		desktopPane.setBackground(Color.BLACK);
-		desktopPane.setBounds(82, 266, 736, 272);
+		desktopPane.setBounds(69, 231, 736, 257);
 		contentPane.add(desktopPane);
 		
 		JButton btnNewButton_4 = new JButton("Titanic");
@@ -222,29 +222,29 @@ public class Principal extends JFrame {
 		});
 		button.setBackground(new Color(255, 255, 255));
 		button.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\usuario1.png"));
-		button.setBounds(901, 0, 129, 115);
+		button.setBounds(901, 0, 129, 107);
 		contentPane.add(button);
 		
 		JLabel lblComprasPopulares = new JLabel("M\u00C1S VECES COMPRADAS");
 		lblComprasPopulares.setForeground(new Color(204, 204, 51));
 		lblComprasPopulares.setFont(new Font("Mongolian Baiti", Font.BOLD, 24));
-		lblComprasPopulares.setBounds(82, 210, 414, 52);
+		lblComprasPopulares.setBounds(69, 174, 414, 52);
 		contentPane.add(lblComprasPopulares);
 		
 		JLabel lblTopTendencias = new JLabel("TOP 5 TENDENCIAS AHORA");
 		lblTopTendencias.setForeground(new Color(204, 204, 51));
 		lblTopTendencias.setFont(new Font("Mongolian Baiti", Font.BOLD, 24));
-		lblTopTendencias.setBounds(82, 539, 414, 52);
+		lblTopTendencias.setBounds(69, 490, 414, 52);
 		contentPane.add(lblTopTendencias);
 		
 		JDesktopPane desktopPane_1 = new JDesktopPane();
 		desktopPane_1.setBackground(new Color(0, 0, 0));
-		desktopPane_1.setBounds(82, 595, 893, 285);
+		desktopPane_1.setBounds(69, 543, 893, 278);
 		contentPane.add(desktopPane_1);
 		
 		JLabel label = new JLabel("01");
 		label.setFont(new Font("Mongolian Baiti", Font.PLAIN, 45));
-		label.setBounds(64, 232, 46, 64);
+		label.setBounds(65, 221, 46, 64);
 		desktopPane_1.add(label);
 		label.setForeground(new Color(204, 204, 51));
 		
@@ -263,7 +263,7 @@ public class Principal extends JFrame {
 			}
 		});
 		btnNewButton_4_2.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Lupin.jpg"));
-		btnNewButton_4_2.setBounds(0, 0, 169, 245);
+		btnNewButton_4_2.setBounds(0, 0, 169, 227);
 		desktopPane_1.add(btnNewButton_4_2);
 		
 		JButton btnNewButton_4_1_2 = new JButton("");
@@ -281,7 +281,7 @@ public class Principal extends JFrame {
 			}
 		});
 		btnNewButton_4_1_2.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\La casa de papel.jpg"));
-		btnNewButton_4_1_2.setBounds(171, 0, 184, 245);
+		btnNewButton_4_1_2.setBounds(171, 0, 184, 227);
 		desktopPane_1.add(btnNewButton_4_1_2);
 		
 		JButton btnNewButton_4_1_1_1 = new JButton("");
@@ -299,7 +299,7 @@ public class Principal extends JFrame {
 			}
 		});
 		btnNewButton_4_1_1_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Venom.jpg"));
-		btnNewButton_4_1_1_1.setBounds(356, 0, 177, 245);
+		btnNewButton_4_1_1_1.setBounds(356, 0, 177, 227);
 		desktopPane_1.add(btnNewButton_4_1_1_1);
 		
 		JButton btnNewButton_5_1 = new JButton("");
@@ -317,7 +317,7 @@ public class Principal extends JFrame {
 			}
 		});
 		btnNewButton_5_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Blacklist.jpg"));
-		btnNewButton_5_1.setBounds(533, 0, 189, 245);
+		btnNewButton_5_1.setBounds(533, 0, 189, 227);
 		desktopPane_1.add(btnNewButton_5_1);
 		
 		JButton btnNewButton_5_1_1 = new JButton("");
@@ -335,45 +335,45 @@ public class Principal extends JFrame {
 			}
 		});
 		btnNewButton_5_1_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\The good doctor.jpg"));
-		btnNewButton_5_1_1.setBounds(723, 0, 169, 245);
+		btnNewButton_5_1_1.setBounds(723, 0, 169, 227);
 		desktopPane_1.add(btnNewButton_5_1_1);
 		
 		JLabel label_1 = new JLabel("02");
 		label_1.setForeground(new Color(204, 204, 51));
 		label_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 45));
-		label_1.setBounds(229, 232, 46, 64);
+		label_1.setBounds(226, 221, 46, 64);
 		desktopPane_1.add(label_1);
 		
 		JLabel label_1_1 = new JLabel("03");
 		label_1_1.setForeground(new Color(204, 204, 51));
 		label_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 45));
-		label_1_1.setBounds(410, 232, 46, 64);
+		label_1_1.setBounds(412, 221, 46, 64);
 		desktopPane_1.add(label_1_1);
 		
 		JLabel label_1_1_1 = new JLabel("04");
 		label_1_1_1.setForeground(new Color(204, 204, 51));
 		label_1_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 45));
-		label_1_1_1.setBounds(618, 232, 46, 64);
+		label_1_1_1.setBounds(613, 221, 46, 64);
 		desktopPane_1.add(label_1_1_1);
 		
 		JLabel label_1_1_1_1 = new JLabel("05");
 		label_1_1_1_1.setForeground(new Color(204, 204, 51));
 		label_1_1_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 45));
-		label_1_1_1_1.setBounds(778, 232, 46, 64);
+		label_1_1_1_1.setBounds(776, 221, 46, 64);
 		desktopPane_1.add(label_1_1_1_1);
 		
 		JLabel lblNewLabel = new JLabel("@KapitalFilms_");
 		lblNewLabel.setFont(new Font("Mongolian Baiti", Font.PLAIN, 16));
 		lblNewLabel.setForeground(new Color(204, 204, 51));
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Logo Insta.jpg"));
-		lblNewLabel.setBounds(459, 883, 147, 20);
+		lblNewLabel.setBounds(523, 858, 147, 20);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("@KapitalFilms_");
 		lblNewLabel_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 16));
 		lblNewLabel_1.setForeground(new Color(204, 204, 51));
 		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Logo twitter.png"));
-		lblNewLabel_1.setBounds(459, 919, 147, 20);
+		lblNewLabel_1.setBounds(523, 894, 147, 20);
 		contentPane.add(lblNewLabel_1);
 		
 		JButton btnBuscadorDeArticulos = new JButton("BUSCADOR DE ARTICULOS");
@@ -389,7 +389,12 @@ public class Principal extends JFrame {
 				
 			}
 		});
-		btnBuscadorDeArticulos.setBounds(586, 131, 245, 46);
+		btnBuscadorDeArticulos.setBounds(586, 113, 245, 46);
 		contentPane.add(btnBuscadorDeArticulos);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Alumno\\Downloads\\logo.PNG"));
+		lblNewLabel_2.setBounds(290, 837, 193, 98);
+		contentPane.add(lblNewLabel_2);
 	}
 }
