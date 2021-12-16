02e0210e97d9a8339cf77c7ecdfae2e457e1b2da
aritznieto <aritz.nieto@opendeusto.es>
2021-12-16 12:11:58 +0100

    palpala

--- src/ventanas/ListaPeliculas.java
+++ src/ventanas/ListaPeliculas.java
@@ -21,6 +21,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import java.awt.Font;
 import javax.swing.JLabel;
+import java.awt.SystemColor;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class ListaPeliculas extends JFrame {
 
@@ -36,8 +39,9 @@ public class ListaPeliculas extends JFrame {
 	 */
 	public ListaPeliculas(Usuario u) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 552, 472);
+		setBounds(100, 100, 762, 588);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -48,7 +52,7 @@ public class ListaPeliculas extends JFrame {
 		
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(41, 75, 271, 292);
+		scrollPane.setBounds(227, 154, 271, 292);
 		contentPane.add(scrollPane);
 		scrollPane.setViewportView(list);
 		
@@ -59,7 +63,10 @@ public class ListaPeliculas extends JFrame {
 		}
 		list.setModel(peliculas);
 		
-		JButton btnAlquilar = new JButton("Alquilar");
+		JButton btnAlquilar = new JButton("ALQUILAR");
+		btnAlquilar.setBackground(new Color(0, 0, 0));
+		btnAlquilar.setFont(new Font("Mongolian Baiti", Font.PLAIN, 18));
+		btnAlquilar.setForeground(new Color(204, 204, 51));
 		btnAlquilar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (u == null){
@@ -90,8 +97,40 @@ public class ListaPeliculas extends JFrame {
 				}
 			}
 		});
-		btnAlquilar.setBounds(400, 371, 115, 29);
+		btnAlquilar.setBounds(292, 474, 142, 29);
 		contentPane.add(btnAlquilar);
+		
+		JLabel lblKapitalfilms = new JLabel("KAPITAL-FILMS");
+		lblKapitalfilms.setForeground(new Color(204, 204, 51));
+		lblKapitalfilms.setFont(new Font("Mongolian Baiti", Font.BOLD | Font.ITALIC, 58));
+		lblKapitalfilms.setBackground(SystemColor.menu);
+		lblKapitalfilms.setBounds(95, 0, 535, 68);
+		contentPane.add(lblKapitalfilms);
+		
+		JLabel lblNewLabel = new JLabel("PEL\u00CDCULAS:");
+		lblNewLabel.setFont(new Font("Mongolian Baiti", Font.PLAIN, 25));
+		lblNewLabel.setForeground(new Color(204, 204, 51));
+		lblNewLabel.setBounds(292, 97, 170, 41);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\logo.PNG"));
+		lblNewLabel_1.setBounds(19, 239, 193, 98);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("@KapitalFilms_");
+		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Logo Insta.jpg"));
+		lblNewLabel_2.setForeground(new Color(204, 204, 51));
+		lblNewLabel_2.setFont(new Font("Mongolian Baiti", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(551, 254, 147, 20);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("@KapitalFilms_");
+		lblNewLabel_1_1.setIcon(new ImageIcon("C:\\Users\\Alumno\\Desktop\\Proyecto program3\\proyectoProgram3\\src\\Images\\Logo twitter.png"));
+		lblNewLabel_1_1.setForeground(new Color(204, 204, 51));
+		lblNewLabel_1_1.setFont(new Font("Mongolian Baiti", Font.PLAIN, 16));
+		lblNewLabel_1_1.setBounds(551, 290, 147, 20);
+		contentPane.add(lblNewLabel_1_1);
 		
 		
 		
